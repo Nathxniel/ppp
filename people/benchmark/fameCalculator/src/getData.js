ded305d071905b79b3990cd26ab6a522ee62e2de
nathaniel <oluwaseun.oshunniyi16@imperial.ac.uk>
2017-12-10 22:17:56 +0000

    begin implementing getData.js
    gets trends data for given search term against GH
    todo: prune output data

---
+++ people/benchmark/fameCalculator/src/getData.js
@@ -0,0 +1,54 @@
+/*
+ * getData.js
+ *
+ * collects google trends data 
+ * for given input
+ * 
+ */
+
+var queryIndex = 3;
+
+// check program arguments
+var argc = process.argv.length;
+if (argc <= queryIndex) {
+  // print error code to file
+  throw("no input");
+}
+
+// rebuild input
+var searchStartDate = process.argv[queryIndex - 1];
+var searchQuery = process.argv[queryIndex];
+for (var i = queryIndex + 1; i < argc; i++) {
+  searchQuery += " ";
+  searchQuery += process.argv[i];
+}
+
+// create api object
+const googleTrends = require('google-trends-api');
+
+// query date
+var startDate = new Date(Date.now() - searchStartDate);
+var cat = 3; // category is arts and entertainment
+googleTrends.interestOverTime({ keyword: [searchQuery
+                                         ,"George Harrison"]
+                              //, startTime: startDate
+                              , category: cat
+                              })
+.then(function(results) {
+  // print result averages to file
+  console.log(results);
+})
+.catch(function(err) {
+  // print error code to file
+});
+
+/*
+}, function(err, results) {
+  if (err) {
+    // print error code to file
+  } else {
+    // print result averages to file
+    console.log(results);
+  }
+});
+*/
