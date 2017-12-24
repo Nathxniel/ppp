38e754230c87b0370533b9da6507b2b3a3d17519
nathaniel <oluwaseun.oshunniyi16@imperial.ac.uk>
2017-12-24 15:53:03 +0000

    update fameCalculator
    fameCalculator can now run from anywhere as per commit 1d52a6b

--- people/benchmark/fameCalculator/src/Fame/NetProcess.hs
+++ people/benchmark/fameCalculator/src/Fame/NetProcess.hs
@@ -5,6 +5,7 @@ import System.Process  ( StdStream(..)
                        , CreateProcess(..)
                        , proc
                        )
+import System.Directory ( getUserDocumentsDirectory )
 import System.IO (hGetContents, hClose)
 
 type Query = String
@@ -13,23 +14,25 @@ type Request = String
 
 getRequest :: Query -> Time -> IO (Request)
 getRequest q t = do
+  home <- getUserDocumentsDirectory
   (_, Just hout, _, _) <- createProcess $
                           (proc "node" ["getData.js"
                                        ,t
                                        ,q
                                        ]
-                          ) { cwd     = Nothing
+                          ) { cwd     = Just (home ++ "/.fame/")
                             , std_out = CreatePipe }
   out <- hGetContents hout
   return out
 
 getQuery :: String -> IO (Query)
 getQuery s = do
+  home <- getUserDocumentsDirectory
   (_, Just hout, _, _) <- createProcess $
                           (proc "node" ["getInput.js"
                                        ,s
                                        ]
-                          ) { cwd     = Nothing
+                          ) { cwd     = Just (home ++ "/.fame/")
                             , std_out = CreatePipe }
   out <- hGetContents hout
   return out
