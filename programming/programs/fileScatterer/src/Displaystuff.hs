f433e746a46f38c99bbf2fa19a62dd25bde4ecac
Nathxniel <oluwaseun.oshunniyi16@imperial.ac.uk>
2018-03-18 04:26:55 +0000

    begin implement scriptability
    finished sugaring welcome screen

--- programming/programs/fileScatterer/src/Displaystuff.hs
+++ programming/programs/fileScatterer/src/Displaystuff.hs
@@ -7,14 +7,49 @@ import qualified System.Console.Terminal.Size as Terminal
 
 getWidth = Terminal.width . (maybe (error "Terminal error") id)
 
-welcome :: IO ()
-welcome = do
-  width <- getWidth <$> Terminal.size
+simpleHello :: Int -> String -> IO ()
+simpleHello width stat = do
   let hwidth  = width `div` 2;
       bar     = replicate hwidth '#';
       midbar  = strPadBoth ' ' width bar;
-      text    = "file scatterer";
+  putStrLn midbar
+  printmid width stat
+  putStrLn midbar
+
+printmid :: Int -> String -> IO ()
+printmid width stat = do
+  let hwidth  = width `div` 2;
+      text    = stat
       text'   = strPadBoth ' ' (hwidth - 4) text;
       text''  = strPadBoth '#' hwidth text';
       midtext = strPadBoth ' ' width text''
-  putStrLn . unlines $ [midbar, midtext, midbar]
+  putStrLn midtext
+
+welcome :: IO ()
+welcome = do
+  width <- getWidth <$> Terminal.size
+  let say = printmid width
+  if width < 102
+  then do
+    simpleHello width "file scatterer"
+  else do
+    mapM_ say $
+      ["###################################################################################################"
+      ,"#        [c.ccccc.[c.[cc   .[cccccc                                                               #"
+      ,"#        [c.     .[c.[cc   .[cc                                                                   #"
+      ,"#        [c.     .[c.[cc   .[cc                                                                   #"
+      ,"#        [c.cccc .[c.[cc   .[ccccc                                                                #"
+      ,"#        [c.     .[c.[cc   .[cc                                                                   #"
+      ,"#        [c.     .[c.[cc   .[cc                                                                   #"
+      ,"#        [c.     .[c.[ccccc.[cccccc                                                               #"
+      ,"#                                                                                                 #"
+      ,"#          [cc cc  ...[cc     ..[c..cccc[ccc.[ccc[cccc.[ccccc.[ccccccc  .[cccccc.[ccccccc         #"
+      ,"#        [cc    [..[cc   [c  ..[c cc   .[cc     .[cc  .[cc   .[cc    [cc.[cc    .[cc    [cc       #"
+      ,"#         [cc    .[cc       ..[c  [cc  .[cc     .[cc  .[cc   .[cc    [cc.[cc    .[cc    [cc       #"
+      ,"#           [cc  .[cc      ..[cc   [cc .[cc     .[cc  .[ccccc.[c [cc    .[cccccc.[c [cc           #"
+      ,"#              [c.[cc     ..[cccccc [cc.[cc     .[cc  .[cc   .[cc  [cc  .[cc    .[cc  [cc         #"
+      ,"#        [cc    [..[cc   [c[cc       [c.[cc     .[cc  .[cc   .[cc    [cc.[cc    .[cc    [cc       #"
+      ,"#          [cc cc ...[cccc cc         [.[cc     .[cc  .[ccccc.[cc      [.[cccccc.[cc      [cc     #"
+      ,"###################################################################################################"
+      ]
+
