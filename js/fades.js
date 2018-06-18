ca21a2aad73529981dc578d891ed108957a0689b
nathaniel <oluwaseun.oshunniyi16@imperial.ac.uk>
2018-06-18 09:43:15 +0100

    random site contributions

--- js/fades.js
+++ js/fades.js
@@ -5,6 +5,25 @@ function fade(seconds) {
 
 function fadeIn(seconds) {
   $(document).ready(function(){
-    fade(seconds);
+  var sites = 3;
+  var rand = Math.floor((Math.random() * sites) + 1);
+  var backtext = '<img id="face" alt-text="me" src="./pictures/face-ass.jpg"></img><h1 id="dotcom">nathaniel oshunniyi</h1></a>';
+
+  // dailytelegram, icl ai, komi
+    if (rand == 1) {
+      backtext = '<a href="https://thedailytelegram.co.uk">'
+               + backtext;
+
+    } else if (rand == 2) {
+      backtext = '<a href="https://komimagazine.club">'
+               + backtext;
+
+    } else if (rand == 3) {
+      backtext = '<a href="http://www.mlsoc.ai/">'
+               + backtext;
+
+    };
+    $('.title').html(backtext);
   });
+  fade(seconds);
 }
